311b34b7f11e809bd131626770dc34a3a25237ba
Rachel Tao <rt1726@nyu.edu>
2022-04-20 22:25:25 -0400

    ADD stage selection

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,13 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
+using UnityEditor;
 
 public class MainMenu : MonoBehaviour
 {
+    string next_map_path;
+    string next_song_path;
     
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
         //GameStateManager.Instance.SetState(GameState.Gameplay);
+    }
+
+    public void StartStage1() {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void StartStage2() {
+        next_map_path = "Assets/Maps/NewTest.txt";
+        next_song_path = "Music/THE-PRIMALS";
+
+        string map = File.ReadAllText(next_map_path);
+        StageController.map = map;
+
+        AudioClip audioClip = Resources.Load<AudioClip>(next_song_path);
+        StageController.musicClip = audioClip;
+
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
